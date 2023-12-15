b8063c830e7401323e71c16c9791f91ae5f82cac
Tori <toriroettger@gmail.com>
2023-12-15 17:09:13 -0600

    fix: bryptjs import

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const { JWT_SECRET = "SECRET_KEY" } = process.env;
