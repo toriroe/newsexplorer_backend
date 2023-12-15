1478c7bb85aabce3cce32479490d29ceb69e713b
Tori <toriroettger@gmail.com>
2023-12-15 16:07:34 -0600

    create 'user' controllers, create 'findUserByCredentials' userSchema function

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,8 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { JWT_SECRET = "SECRET_KEY" } = process.env;
 
 const NotFoundError = require("../errors/not-found");
 const ConflictError = require("../errors/conflict");
@@ -6,28 +10,62 @@ const UnauthorizedError = require("../errors/unauthorized");
 const BadRequestError = require("../errors/bad-request");
 
 const createUser = (req, res, next) => {
-  // const { email, name, password } = req.body;
-  // User.findOne({ email }).then((emailFound) => {
-  //   if (emailFound) {
-  //     throw new ConflictError("Email already exists");
-  //   } else {
-  //     User.create({ name, email, password }).then((user) => {});
-  //   }
-  // });
+  const { email, name, password } = req.body;
+  User.findOne({ email })
+    .then((emailFound) => {
+      if (emailFound) {
+        throw new ConflictError("Email already exists");
+      } else {
+        bcrypt
+          .hash(password, 10)
+          .then((hash) => {
+            User.create({ name, email, password: hash })
+              .then((user) => {
+                const token = jwt.sign({ _id: user.id }, JWT_SECRET, {
+                  expireIn: "7d",
+                });
+                res.status(201).send({ name, email, _id: user._id, token });
+              })
+              .catch((err) => {
+                if (err.name === "ValidationError") {
+                  next(new BadRequestError("Validation Error"));
+                } else {
+                  next(err);
+                }
+              });
+          })
+          .catch((err) => {
+            next(err);
+          });
+      }
+    })
+    .catch(next);
 };
 
-const login = () => {};
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user.id }, JWT_SECRET, { expiresIn: "7d" });
+      res.send({ name: user.name, email, _id: user.id, token });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(new UnauthorizedError("Error from login"));
+    });
+};
 
 const getCurrentUser = (req, res, next) => {
-  // const userId = req.user._id;
-  // User.findById(userId)
-  //   .then((user) => {
-  //     if (!user) {
-  //       throw new NotFoundError("No user with matching ID found");
-  //     }
-  //     res.send(user);
-  //   })
-  //   .catch(next);
+  const userId = req.user._id;
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError("No user with matching ID found");
+      }
+      res.send(user);
+    })
+    .catch(next);
 };
 
 module.exports = { createUser, login, getCurrentUser };
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,5 +26,26 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user || !user.password) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
