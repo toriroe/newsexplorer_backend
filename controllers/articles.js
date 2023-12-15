d6e5039932876850566870e5ebe890c78c1fcf51
Tori <toriroettger@gmail.com>
2023-12-15 16:27:11 -0600

    create article controllers

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,9 +1,39 @@
 const Article = require("../models/article");
 
-const addArticle = () => {};
+const NotFoundError = require("../errors/not-found");
+const BadRequestError = require("../errors/bad-request");
 
-const removeArticle = () => {};
+const addArticle = (req, res, next) => {
+  const owner = req.user._id;
+  const { keyword, title, text, date, source, link, image } = req.body;
 
-const getArticles = () => {};
+  Article.create({ keyword, title, text, date, source, link, image, owner })
+    .then((article) => {
+      res.status(201).send({ data: article });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Validation Error"));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const removeArticle = (req, res, next) => {
+  const { articleId } = req.params;
+
+  Article.findByIdAndRemove(articleId)
+    .orFail(() => new NotFoundError("Error from removeArticle"))
+    .then((removedArticle) => res.send(removedArticle))
+    .catch(next);
+};
+
+const getArticles = (req, res, next) => {
+  Article.find({})
+    .then((articles) => res.send(articles))
+    .catch(next);
+};
 
 module.exports = { addArticle, removeArticle, getArticles };
