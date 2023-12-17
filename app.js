b0c99b641e30dc38eae8dc958252ac4729b1029f
Tori <toriroettger@gmail.com>
2023-12-16 18:22:11 -0600

    add rate limiter, install helmet and cors

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const express = require("express");
 
 const { default: mongoose } = require("mongoose");
+const helmet = require("helmet");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+const cors = require("cors");
 const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { limiter } = require("./utils/limiter");
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
@@ -27,6 +30,9 @@ mongoose.connect(
 const routes = require("./routes");
 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
